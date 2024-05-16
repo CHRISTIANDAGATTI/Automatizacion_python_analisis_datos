e1c9640745f20de987d324f14e6d651c62dca2b2
CHRISTIANDAGATTI <dagattichristian@gmail.com>
2024-05-16 19:51:30 -0300

    Requirements + Funciones

--- funciones_archivos.py
+++ funciones_archivos.py
@@ -1,60 +1,189 @@
 import shutil
 import os
+import sys
+import pandas
 from datetime import datetime
+from tqdm import tqdm
 
-def mover_archivos(nombre_archivo_original, nombre_archivo_transformado, nueva_ubicacion_original, nueva_ubicacion_transformado):
+
+
+def leer_csv(nombre_archivo : str, separador: str):
     """
-    Mueve el archivo original a una nueva ubicación y luego mueve el archivo transformado a otra ubicación.
-    Agrega la fecha actual al nombre del archivo en el formato 'yyyy-mm-dd' después de moverlos.
+    Lee un archivo CSV y devuelve un DataFrame.
 
     Parámetros:
-    nombre_archivo_original (str): El nombre del archivo original.
-    nombre_archivo_transformado (str): El nombre del archivo transformado.
-    nueva_ubicacion_original (str): La nueva ubicación para el archivo original.
-    nueva_ubicacion_transformado (str): La nueva ubicación para el archivo transformado.
+    nombre_archivo (str): El nombre del archivo CSV a leer.
+    separador (str): El separador de campos en el archivo CSV.
 
     Returns:
-    bool: True si la operación fue exitosa, False si hubo un error.
+    df (pd.DataFrame): El DataFrame resultante.
+    """
+    # Obtén el número total de líneas del archivo
+    num_lines = sum(1 for l in open(nombre_archivo, 'r', encoding='utf-8'))
+
+    # Define el tamaño del chunk
+    chunk_size = 10000
+
+    # Crea un objeto tqdm para mostrar la barra de progreso
+    progress_bar = tqdm(total=num_lines, desc="Leyendo CSV", bar_format='{l_bar}{bar:30}{r_bar}', colour='yellow')
+
+    # Lee el archivo CSV en chunks y actualiza la barra de progreso
+    chunks = []
+    for chunk in pandas.read_csv(nombre_archivo, chunksize=chunk_size, sep=separador, encoding='utf-8'):
+        chunks.append(chunk)
+        progress_bar.update(chunk_size)
+
+    # Cierra la barra de progreso
+    progress_bar.close()
+
+    # Combina los chunks en un solo DataFrame
+    df = pandas.concat(chunks, axis=0)
+
+    return df
+
+
+
+#   mover_archivos('entrada.csv'          , 'salida.csv'               , 'paraBackup'            , 'paraAnalisis')
+def mover_archivos(nombre_archivo_entrada, nombre_archivo_salida, nueva_ubicacion_original, nueva_ubicacion_transformado):
+    try:
+        
+        # ------------------------ DEFINICIONES DE NOMBRES ARCHIVOS Y DIRECTORIOS ------------------------------------
+  
+        # print("Ubicación del ejecutable: ", sys.executable)
+        # cuando se va a generar un ejectuable hay que usar sys.executable ya que el ejecutable genera un directorio temporal y 
+        # se pierde la ubicacion del archivo por lo que hay que descomentar la siguiente linea para generar el ejecutable
+        # base_dir = os.path.dirname(sys.executable)  ## esto sirve para hacer el ejecutable
+        
+        # cuando no se usa un archivo ejecutable se debe usar la siguiente linea, si se va generar el ejecutable
+        # base_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+        #  ESTO LO QUE HACE EN ASIGNAR DINAMICA EL BASE_DIR, SI SE ESTA EJECUTANDO UN SCRIPT ENTONCES USA LA SEGUNDA LINEA DEL IF SI SE ESTA EJECUTNADO
+        # UN JECUTABLE ENTONCES USA LA PRIMER LINEA DEL IF
+        if getattr(sys, 'frozen', False):
+            # El programa se está ejecutando como un ejecutable empaquetado con PyInstaller
+            base_dir = os.path.dirname(sys.executable)
+        else:
+            # El programa se está ejecutando como un script
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+        # './entrada.csv'
+        # directorio_original_y_nombre_entrada = os.path.normpath(nombre_archivo_entrada)
+        # print("nombre archivo entrada", directorio_original_y_nombre_entrada)
+        directorio_original_y_nombre_entrada = os.path.normpath(os.path.join(base_dir, nombre_archivo_entrada))
+        
+        # './salida.csv' 
+        # directorio_original_y_nombre_salida = os.path.normpath(nombre_archivo_salida)  
+        # print("nombre archivo salida", directorio_original_y_nombre_salida)      
+        directorio_original_y_nombre_salida = os.path.normpath(os.path.join(base_dir, nombre_archivo_salida))
+
+        # './paraBackup' 
+        # directorio_paraBackup = os.path.normpath(nueva_ubicacion_original)  
+        # print("directorio para backup ", directorio_paraBackup)     
+        directorio_paraBackup = os.path.normpath(os.path.join(base_dir, nueva_ubicacion_original))
+        
+        # './paraAnalisis'
+        # directorio_paraAnalisis = os.path.normpath(nueva_ubicacion_transformado)
+        # print("directorio para analisis ", directorio_paraAnalisis)        
+        directorio_paraAnalisis = os.path.normpath(os.path.join(base_dir, nueva_ubicacion_transformado))
+
+
+        # ---------------------- CREACION DIRECTORIOS DESTINO y COPIA ARCHIVOS ORIGINALES-------------------------------------------
+        # './paraBackup' 
+        os.makedirs(directorio_paraBackup, exist_ok=True)
+        
+        # './paraAnalisis'
+        os.makedirs(directorio_paraAnalisis, exist_ok=True)
+
+
+        # print("inicio copia archivos originales")
+        # copia './entrada.csv'  en la ruta './paraBackup' 
+        # shutil.copy2(directorio_original_y_nombre_entrada, directorio_paraBackup)
+        
+        # copia './salida.csv'  en la ruta './paraAnalisis'
+        # shutil.copy2(directorio_original_y_nombre_salida, directorio_paraAnalisis)
+        
+        # print("fin copia archivos originales")
+        # input("presione enter para continuar")
+        
+        
+        # ---------------------- RENOMBRADO ARCHIVOS-------------------------------------------
+
+        # obtengo la fecha actual por ej 2024-05-13
+        fecha_actual = datetime.now().strftime('%Y-%m-%d')
+
+        # genero una variable con el nombre:   2024-05-13_entrada.csv
+        nombre_archivo_fecha_entrada_csv = f"{fecha_actual}_{os.path.basename(nombre_archivo_entrada)}"
+       
+        
+        # genero una variable con el nombre:  2024-05-13_salida.csv
+        nombre_archivo_fecha_salida_csv = f"{fecha_actual}_{os.path.basename(nombre_archivo_salida)}"
+       
+
+
+        # genero una variable con el nombre: ./paraBackup/2024-05-13_entrada.csv
+        directorio_paraBackup_fecha_entrada_csv = os.path.join(directorio_paraBackup, nombre_archivo_fecha_entrada_csv)
+        
+        
+        # genero una varialbe con el nombre ./paraAnalisis/2024-05-13_salida.csv
+        directorio_paraAnalisis_fecha_salida_csv = os.path.join(directorio_paraAnalisis, nombre_archivo_fecha_salida_csv)
+        
+        
+
+        # Si el archivo de destino ya existe, se borra
+        # ./paraBackup/2024-05-13_entrada.csv
+        if os.path.exists(directorio_paraBackup_fecha_entrada_csv):
+            os.remove(directorio_paraBackup_fecha_entrada_csv)    
+   
+
+       
+        #     ./entrada.csv    , ./paraBackup/2024-05-13_entrada.csv
+        shutil.copy2(directorio_original_y_nombre_entrada, directorio_paraBackup_fecha_entrada_csv)
+        os.remove(directorio_original_y_nombre_entrada)  
+        
+        
+            
+
+        # Si el archivo de destino ya existe, se borra 
+        # ./paraAnalisis/2024-05-13_salida.csv
+        if os.path.exists(directorio_paraAnalisis_fecha_salida_csv):
+            os.remove(directorio_paraAnalisis_fecha_salida_csv)
+            
+
+        #            './entrada.csv'                    , 
+        shutil.copy2(directorio_original_y_nombre_salida, directorio_paraAnalisis_fecha_salida_csv )        
+        os.remove(directorio_original_y_nombre_salida)
+
+        return True
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+    
+
+
+def generar_archivo_csv(df: pandas.DataFrame, nombre_archivo: str):
+    """
+    Genera un archivo CSV a partir de un DataFrame.
+
+    Parámetros:
+    dataframe (pd.DataFrame): El DataFrame a convertir en CSV.
+    nombre_archivo (str): El nombre del archivo CSV a generar.
+
+    Returns:
+    None
 
     """
     try:
-        # Obtener la ruta al directorio del script
-        base_dir = os.path.dirname(__file__)
+        # Crear una barra de progreso
+        with tqdm(total=len(df), desc="Generando CSV", ncols=100, bar_format='{l_bar}{bar:30}{r_bar}', colour='yellow') as pbar:
+            for index, row in df.iterrows():
+                # Actualizar la barra de progreso
+                pbar.update()
+                # Aquí puedes hacer cualquier procesamiento que necesites con la fila
 
-        # Construir las rutas completas de los archivos
-        ruta_original = os.path.normpath(os.path.join(base_dir, nombre_archivo_original))
-        ruta_nueva_original = os.path.normpath(os.path.join(base_dir, nueva_ubicacion_original, nombre_archivo_original))
-        ruta_transformado = os.path.normpath(os.path.join(base_dir, nombre_archivo_transformado))
-        ruta_nueva_transformado = os.path.normpath(os.path.join(base_dir, nueva_ubicacion_transformado, nombre_archivo_transformado))
-
-        # Crear los directorios si no existen
-        os.makedirs(os.path.dirname(ruta_nueva_original), exist_ok=True)
-        os.makedirs(os.path.dirname(ruta_nueva_transformado), exist_ok=True)
-
-        # Mover el archivo original a la nueva ubicación
-        shutil.move(ruta_original, ruta_nueva_original)
-
-        # Mover el archivo transformado a la nueva ubicación
-        shutil.move(ruta_transformado, ruta_nueva_transformado)
-
-        # Obtener la fecha actual en el formato 'yyyy-mm-dd'
-        fecha_actual = datetime.now().strftime('%Y-%m-%d')
-
-        # Agregar la fecha actual al nombre del archivo
-        nombre_archivo_original_con_fecha = f"{fecha_actual}_{nombre_archivo_original}"
-        nombre_archivo_transformado_con_fecha = f"{fecha_actual}_{nombre_archivo_transformado}"
-
-        # Construir las rutas completas de los archivos con la fecha
-        ruta_nueva_original_con_fecha = os.path.normpath(os.path.join(base_dir, nueva_ubicacion_original, nombre_archivo_original_con_fecha))
-        ruta_nueva_transformado_con_fecha = os.path.normpath(os.path.join(base_dir, nueva_ubicacion_transformado, nombre_archivo_transformado_con_fecha))
-
-        # Renombrar los archivos para agregar la fecha
-        os.rename(ruta_nueva_original, ruta_nueva_original_con_fecha)
-        os.rename(ruta_nueva_transformado, ruta_nueva_transformado_con_fecha)
-
-        # Si todo salió bien, devolver True
-        return True
+        # Guardar el DataFrame como CSV
+        df.to_csv(nombre_archivo, index=False, sep=';')
+        print(f"Archivo {nombre_archivo} generado con éxito.")
     except Exception as e:
-        # Si hubo un error, devolver False
-        print(f"Error: {e}")
-        return False
+        print(f"Error al generar el archivo CSV: {e}")
--- ingresos.py
+++ ingresos.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import warnings
+import time
+
 
 from funciones_conversion import funcion_string_a_fecha, funcion_string_a_float, funcion_str_to_int, convertir_tipos_datos, funcion_float_to_str
 from funciones_controles import control_cantidad_registros, control_nombre_columnas, verificar_vacios_o_nulos, comparar_tipo_datos_dataframe_csv
@@ -7,6 +10,14 @@ from salida_errores import generar_archivo_errores_o_csv
 from funciones_estadisticas import calcular_estadisticas
 from funciones_pdf import generar_pdf_estadistico, generar_pdf_formatos, generar_pdf_cant_filas_y_nombre_columnas
 
+# Ignorar las advertencias de RuntimeWarning
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+input("El archivo que se va a leer se debe llamar entrada.csv, presione 2 Enter para continuar...")
+# input("Presione enter para continuar")
+
+# Guarda el tiempo de inicio
+start_time = time.time()
 
 #   ------------------------ INICIO DEFINICION VARIABLES ---------------------------------
 
@@ -50,10 +61,7 @@ dtype_dict_esperado = {
 nombre_archivo_csv_entrada = 'CSV_MATARIFE_INGRESOS 202306-202402.csv'
 nombre_archivo_csv_salida = './datos.csv'
 
-#   ------------------------ FIN DEFINICION VARIABLES ---------------------------------
-
-
-
+#   ------------------------ FIN DEFINICION VARIABLES --------------------------------------------------
 
 #   ------------------------ INICIO CREACION LISTAS PARA GUARDAR DATOS ---------------------------------
 
@@ -71,13 +79,12 @@ estadisticas = {}
 
 #   ------------------------ FIN CREACION LISTAS PARA GUARDAR DATOS ---------------------------------
 
+#   ------------------------ INICIO LECTURA ARCHIVOS ------------------------------------------------
 
-
-#   ------------------------ INICIO LECTURA ARCHIVOS ---------------------------------
-
-#leo el archivo csv del sistema de archivos
-
-datos = pd.read_csv(nombre_archivo_csv_entrada, sep=';') 
+#Leo el archivo csv del sistema de archivos
+datos =  leer_csv(nombre_archivo_csv_entrada, ',')
+# datos = pd.read_csv(nombre_archivo_csv_entrada, sep=',') 
+print("Inicio de transformaciones y generacion de archivos Pdf")
 datos = datos.astype(str)
 # monto el drive
 # from google.colab import files
@@ -88,8 +95,7 @@ datos = datos.astype(str)
 # datos = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/Faena/CSV_MATARIFE_FAENA 202306- 202402_ QUINTA PARTE.csv', sep=';', dtype=dtype_dict )
 
 
-#   ------------------------ FIN LECTURA ARCHIVOS ---------------------------------
-
+#   ------------------------ FIN LECTURA ARCHIVOS ----------------------------------------
 
 #   ------------------------ INICIO TRANSFORMACION DATOS ---------------------------------
 
@@ -106,8 +112,7 @@ datos = funcion_str_to_int(datos, errores_transformacion_datos, 'Establecimiento
 
 #   ------------------------ FIN TRANSFORMACION DATOS ---------------------------------
 
-
-#   ------------------------ INICIO CONTROLES ---------------------------------
+#   ------------------------ INICIO CONTROLES -----------------------------------------
 
 #comparacion de tipo de datos y los resultados los envia a --> resultados_formato
 comparar_tipo_datos_dataframe_csv(datos, resultados_formato, dtype_dict_esperado)
@@ -121,20 +126,18 @@ control_nombre_columnas(datos, dtype_dict_esperado, resultados_cant_columnas_y_n
 #control vacios nulos  y los resultados los envia a --> estadisticas
 # verificar_vacios_o_nulos(datos, estadisticas)
 
-#   ------------------------ FINAL CONTROLES ---------------------------------
-
-
+#   ------------------------ FINAL CONTROLES -------------------------------------
 
 #   ------------------------ INICIO ESTADISTICAS ---------------------------------
+
 
 #calculos estadisticas
 calcular_estadisticas(datos, estadisticas)
 
-#   ------------------------ FIN ESTADISTICAS ---------------------------------
 
- 
+#   ------------------------ FIN ESTADISTICAS --------------------------------------
 
-#   ------------------------ INICIO GENERAR PDF ---------------------------------
+#   ------------------------ INICIO GENERAR PDF ------------------------------------
 
 generar_pdf_estadistico(estadisticas, 'estadisticas.pdf', 15, 'Reporte de estadisticas')
 
@@ -144,18 +147,37 @@ generar_pdf_formatos(resultados_formato, 'formatos.pdf', 'Reporte de formatos')
 
 # esta funcion genera pdf donde muestra si hubo columnas que no existen o la cant de filas de entrada y salida son distintas
 generar_pdf_cant_filas_y_nombre_columnas(resultados_cant_columnas_y_nombre_columnas, 'cant_filas_y_nombre_columnas.pdf', 'Reporte de cantidad de filas y nombre de columnas')
+print("Fin de transformaciones y generacion de archivos Pdf")
 
-#   ------------------------ FIN GENERAR PDF ---------------------------------
+
+#   ------------------------ FIN GENERAR PDF ---------------------------------------
+
+#   ------------------------ INICIO CREAR CSV --------------------------------------
+
+# en este punto tengo 2 archivos 1) entrada.csv 2) salida.csv (Este ultimo tiene todos los datos transformados)
+generar_archivo_csv(datos, nombre_archivo_csv_salida)
 
 
+
+#   ------------------------ FIN CREAR CSV -----------------------------------------
 
 #   ------------------------ INICIO MOVER ARCHIVOS ---------------------------------
 
 
 #TODO si existe algun error hacer la funcionalidad de no mover el archivo, consultar que error son los importantes
 
+print("Inicio de movimiento de archivos")
 # mover archivos, se almacena en una variable booleana
-# resultado_mover_archivos = mover_archivos(nombre_archivo_csv_entrada, nombre_archivo_csv_salida, './procesado', './paraAnalisis')
+resultado_mover_archivos = mover_archivos(nombre_archivo_csv_entrada, nombre_archivo_csv_salida, 'paraBackup', 'paraAnalisis')
+
+print("Fin de movimiento de archivos")
+
+# Guarda el tiempo de finalización
+end_time = time.time()
+
+duration = end_time - start_time
+print(f"El script tardó {round(duration/60, 2)} minutos en completarse.")
+input("Presione 2 Enter para terminar...")
 
 #   ------------------------ FIN MOVER ARCHIVOS ---------------------------------
 
