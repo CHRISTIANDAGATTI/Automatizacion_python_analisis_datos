f4664321a41cafef30d715feaf82396b8174ee55
CHRISTIANDAGATTI <dagattichristian@gmail.com>
2024-05-22 08:22:32 -0300

    Calcular media solo si es no nulo

--- funciones_estadisticas.py
+++ funciones_estadisticas.py
@@ -1,7 +1,7 @@
-import pandas as pd
+import pandas
 import numpy as np
 
-def calcular_estadisticas(df, estadisticas):
+def calcular_estadisticas(df: pandas.DataFrame , estadisticas : dict):
     """
     Calcula estadísticas para cada columna numérica en un DataFrame de pandas.
 
@@ -31,16 +31,18 @@ def calcular_estadisticas(df, estadisticas):
 
     # Calcular las estadísticas para cada columna numérica
     for columna in columnas_numericas:
-        maximo = round(df[columna].max(), 2) if not pd.isna(df[columna].max()) else np.nan
-        minimo = round(df[columna].min(), 2) if not pd.isna(df[columna].min()) else np.nan
+        maximo = round(df[columna].max(), 2) if not pandas.isna(df[columna].max()) else np.nan
+        minimo = round(df[columna].min(), 2) if not pandas.isna(df[columna].min()) else np.nan
         
-        #si la columna tiene valores y no  tienen nan entonces se calcula la media
-        if df[columna].count()>0 and not df[columna].isna().all():
-            media = round(df[columna].mean(), 2) if not pd.isna(df[columna].mean()) else np.nan
+        # Si la columna tiene valores y no todos son NaN, entonces se calcula la media
+        if df[columna].count() > 0 and not df[columna].isna().all():
+            media = round(df[columna].mean(), 2)
+        else:
+            media = np.nan
         
-        desviacion_estandar = round(df[columna].std(), 2) if not pd.isna(df[columna].std()) else np.nan
-        mediana = round(df[columna].median(), 2) if not pd.isna(df[columna].median()) else np.nan
-        moda = round(df[columna].mode()[0], 2) if not df[columna].mode().empty and not pd.isna(df[columna].mode()[0]) else np.nan
+        desviacion_estandar = round(df[columna].std(), 2) if not pandas.isna(df[columna].std()) else np.nan
+        mediana = round(df[columna].median(), 2) if not pandas.isna(df[columna].median()) else np.nan
+        moda = round(df[columna].mode()[0], 2) if not df[columna].mode().empty and not pandas.isna(df[columna].mode()[0]) else np.nan
         cantidad_total = df[columna].count()  # Cantidad de valores no nulos
         nulos = df[columna].isnull().sum()  # Cantidad de valores nulos
         repetidos = df[columna].duplicated().sum()  # Cantidad de valores repetidos
@@ -58,38 +60,3 @@ def calcular_estadisticas(df, estadisticas):
         estadisticas['nulos'].append(nulos)  # Agregar la cantidad de nulos al diccionario
         estadisticas['repetidos'].append(repetidos)  # Agregar la cantidad de repetidos al diccionario
         estadisticas['unicos'].append(unicos)  # Agregar la cantidad de únicos al diccionario
-
-"""
- def calcular_estadisticas(df, estadisticas):
-    
-    Calcula estadísticas para cada columna numérica en un DataFrame de pandas.
-
-    Parámetros:
-    df (pandas.DataFrame): El DataFrame para el cual calcular las estadísticas.
-    estadisticas (dict): El diccionario donde se almacenarán las estadísticas.
-
-    Returns:
-    None
-
-    # Obtener columnas numéricas del DataFrame
-    columnas_numericas = df.select_dtypes(include=[np.number]).columns.tolist()
-
-    # Calcular estadísticas para cada columna numérica
-    for columna in columnas_numericas:
-        descripciones = df[columna].describe().to_dict()
-        estadisticas[columna] = {
-            "count": descripciones["count"],
-            "mean": descripciones["mean"],
-            "std": descripciones["std"],
-            "min": descripciones["min"],
-            "25%": descripciones["25%"],
-            "50%": descripciones["50%"],
-            "75%": descripciones["75%"],
-            "max": descripciones["max"],
-            "mode": df[columna].mode().iloc[0] if not df[columna].empty else np.nan,
-            "nulls": df[columna].isnull().sum(),
-            "duplicates": df[columna].duplicated().sum(),
-            "uniques": df[columna].nunique()
-        }
-
-"""
